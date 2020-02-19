ec3b6b4c0a28b27fa27a987cc241eca13ea8c86d
Justin Humm <justin.humm@posteo.de>
2020-02-19 13:42:34 +0100

    download leaflet files and serve them locally

--- flake.nix
+++ flake.nix
@@ -16,11 +16,25 @@
         pkgs = import nixpkgs {
           system = "x86_64-linux";
         };
+        leaflet = rec {
+          version = "1.6.0";
+          js = pkgs.fetchurl {
+            url = "https://unpkg.com/leaflet@${version}/dist/leaflet.js";
+            sha256 = "sha256:1jnadhs874xis8gg91433m29yb1q15ijclmk7nc6pn0g16pi3nkw";
+          };
+          css = pkgs.fetchurl {
+            url = "https://unpkg.com/leaflet@${version}/dist/leaflet.css";
+            sha256 = "sha256:18vn46pcig8aw3yggpfzbcmjbl7z263a7811lf98wkwji44hcws8";
+          };
+        };
       in pkgs.runCommand "wahlkarte" {
         src = ./.;
       } ''
         mkdir -p $out
-        cp index.html $out/
+        cp $src/index.html $out/
+        mkdir -p $out/leaflet@${leaflet.version}/dist
+        ln -s ${leaflet.js}  $out/leaflet@${leaflet.version}/dist/leaflet.js
+        ln -s ${leaflet.css} $out/leaflet@${leaflet.version}/dist/leaflet.css
       '';
 
     };
