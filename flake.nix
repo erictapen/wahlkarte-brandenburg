20f98a27de8cf9c1960f2bdff3ba0ef4e2a80302
Justin Humm <justin.humm@posteo.de>
2020-02-20 01:52:57 +0100

    managed to get polygons into map

--- flake.nix
+++ flake.nix
@@ -35,6 +35,8 @@
         mkdir -p $out/leaflet@${leaflet.version}/dist
         ln -s ${leaflet.js}  $out/leaflet@${leaflet.version}/dist/leaflet.js
         ln -s ${leaflet.css} $out/leaflet@${leaflet.version}/dist/leaflet.css
+        cp $src/generate-boundaries/out.geo.json $out/out.geo.json
+        cp $src/geojson.js $out/
       '';
 
     };
