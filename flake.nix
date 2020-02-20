37d7c302f5373292b32298e9050a0e001b8d8906
Justin Humm <justin.humm@posteo.de>
2020-02-20 19:26:07 +0100

    deploy script

--- flake.nix
+++ flake.nix
@@ -12,32 +12,39 @@
 
       defaultPackage.x86_64-linux = packages.x86_64-linux.wahlkarte;
 
-      packages.x86_64-linux.wahlkarte = let 
+      packages.x86_64-linux = let
         pkgs = import nixpkgs {
           system = "x86_64-linux";
         };
-        leaflet = rec {
-          version = "1.6.0";
-          js = pkgs.fetchurl {
-            url = "https://unpkg.com/leaflet@${version}/dist/leaflet.js";
-            sha256 = "sha256:1jnadhs874xis8gg91433m29yb1q15ijclmk7nc6pn0g16pi3nkw";
+      in rec {
+        wahlkarte = let
+          leaflet = rec {
+            version = "1.6.0";
+            js = pkgs.fetchurl {
+              url = "https://unpkg.com/leaflet@${version}/dist/leaflet.js";
+              sha256 = "sha256:1jnadhs874xis8gg91433m29yb1q15ijclmk7nc6pn0g16pi3nkw";
+            };
+            css = pkgs.fetchurl {
+              url = "https://unpkg.com/leaflet@${version}/dist/leaflet.css";
+              sha256 = "sha256:18vn46pcig8aw3yggpfzbcmjbl7z263a7811lf98wkwji44hcws8";
+            };
           };
-          css = pkgs.fetchurl {
-            url = "https://unpkg.com/leaflet@${version}/dist/leaflet.css";
-            sha256 = "sha256:18vn46pcig8aw3yggpfzbcmjbl7z263a7811lf98wkwji44hcws8";
-          };
-        };
-      in pkgs.runCommand "wahlkarte" {
-        src = ./.;
-      } ''
-        mkdir -p $out
-        cp $src/index.html $out/
-        mkdir -p $out/leaflet@${leaflet.version}/dist
-        ln -s ${leaflet.js}  $out/leaflet@${leaflet.version}/dist/leaflet.js
-        ln -s ${leaflet.css} $out/leaflet@${leaflet.version}/dist/leaflet.css
-        cp $src/generate-boundaries/geojson.js $out/
-      '';
+        in pkgs.runCommand "wahlkarte" {
+          src = ./.;
+        } ''
+          mkdir -p $out
+          cp $src/index.html $out/
+          mkdir -p $out/leaflet@${leaflet.version}/dist
+          ln -s ${leaflet.js}  $out/leaflet@${leaflet.version}/dist/leaflet.js
+          ln -s ${leaflet.css} $out/leaflet@${leaflet.version}/dist/leaflet.css
+          cp $src/generate-boundaries/geojson.js $out/
+        '';
 
+        deploy = pkgs.writeScript "deploy-wahlkarte" ''
+          #! /usr/bin/env bash
+          ${pkgs.openssh}/bin/scp -r ${wahlkarte}/* mmz.erictapen.name:/webroot/mmz.erictapen.name/
+        '';
     };
+  };
 
 }
