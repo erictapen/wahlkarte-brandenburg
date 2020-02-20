b9ec8b6c1de2c29abf52faf5765dbae7208d759d
Justin Humm <justin.humm@posteo.de>
2020-02-20 01:27:41 +0100

    generate geojson for multiple IDs

--- generate-boundaries/src/main.rs
+++ generate-boundaries/src/main.rs
@@ -1,37 +1,45 @@
 use osm_boundaries_utils::build_boundary;
-use osmpbfreader::{Relation, RelationId};
+use osmpbfreader::{OsmId, Relation, RelationId};
 use std::fs::File;
 
-use geojson::{Feature, GeoJson, Geometry};
+use geojson::{Feature, FeatureCollection, GeoJson, Geometry};
 
 fn main() {
+    let osm_ids: Vec<OsmId> = {
+        vec![62353, 1240588, 365821]
+            .iter()
+            .map(|&v| RelationId(v).into())
+            .collect()
+    };
+
     let raw_file = File::open("raw/brandenburg-latest.osm.pbf").expect("Couldn't open pbf file.");
     let mut pbf_reader = osmpbfreader::OsmPbfReader::new(raw_file);
     println!("Decoding pbf file...");
     let objs = pbf_reader
-        .get_objs_and_deps(|obj| {
-            obj.id() == RelationId(62353).into()
-            // || obj.id() == RelationId(1240588).into()
-            // || obj.id() == RelationId(365821).into()
-        })
+        .get_objs_and_deps(|obj| osm_ids.contains(&obj.id()))
         .expect("Failed to decode pbf file.");
 
-    println!("Build boundaries...");
+    println!("Building boundaries...");
 
-    let mittelmark: &Relation = objs
-        .get(&RelationId(62353).into())
-        .unwrap()
-        .relation()
-        .unwrap();
-    let multi_polygon = build_boundary(&mittelmark, &objs).unwrap();
-    // println!("{:?}", multi_polygon);
+    let mut features: Vec<Feature> = Vec::new();
+    for osm_id in osm_ids {
+        let relation: &Relation = objs.get(&osm_id).unwrap().relation().unwrap();
+        let multi_polygon = build_boundary(&relation, &objs).unwrap();
 
-    let geometry = Geometry::new(geojson::Value::from(&multi_polygon));
-    let geojson = GeoJson::Feature(Feature {
+        let geometry = Geometry::new(geojson::Value::from(&multi_polygon));
+        let feature = Feature {
+            bbox: None,
+            geometry: Some(geometry),
+            id: None,
+            properties: None,
+            foreign_members: None,
+        };
+        features.push(feature);
+    }
+
+    let geojson = GeoJson::FeatureCollection( FeatureCollection {
         bbox: None,
-        geometry: Some(geometry),
-        id: None,
-        properties: None,
+        features: features,
         foreign_members: None,
     });
 
