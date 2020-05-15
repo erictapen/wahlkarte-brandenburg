45b967a9396b74eb561bdc432c7bb7a29a3c78cf
Justin Humm <justin.humm@posteo.de>
2020-05-15 15:11:39 +0200

    added legend which shows the name of the Wahlkreis

--- wahlkarte.js
+++ wahlkarte.js
@@ -1,3 +1,56 @@
+var mymap = L.map('mapid').setView([52.392, 13.387], 8);
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    mymap.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Name: </h4>' +  (props ?
+        '<b>' + props.name + '</b>'
+        : 'Hover over a state');
+};
+
+info.addTo(mymap);
+
+
 function loadJSON(path, success, error)
 {
     var xhr = new XMLHttpRequest();
@@ -18,9 +71,12 @@ function loadJSON(path, success, error)
 }
 
 loadJSON('boundaries.json',
-  function(data) { L.geoJSON(data).addTo(mymap); },
-  function(xhr) { console.error(xhr); }
+    function(data) {
+        geojson = L.geoJSON(data, {
+            onEachFeature: onEachFeature
+        }).addTo(mymap);
+    },
+    function(xhr) { console.error(xhr); }
 );
 
-var mymap = L.map('mapid').setView([52.392, 13.387], 8);
 
