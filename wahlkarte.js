0dac0d6cfc4d1b27a766a8e08b51618c5fd664e1
Justin Humm <justin.humm@posteo.de>
2020-05-23 20:40:27 +0200

    format js code with sane-fmt

--- wahlkarte.js
+++ wahlkarte.js
@@ -1,104 +1,101 @@
-var mymap = L.map('mapid').setView([52.392, 13.387], 8);
+var mymap = L.map('mapid').setView([52.392, 13.387], 8)
 
-var geojson = {};
+var geojson = {}
 
 function highlightFeature(e) {
-    var layer = e.target;
+  var layer = e.target
 
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
+  layer.setStyle({
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7,
+  })
 
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-    }
-    info.update(layer.feature.properties);
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront()
+  }
+  info.update(layer.feature.properties)
 }
 
 function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
+  geojson.resetStyle(e.target)
+  info.update()
 }
 
 function zoomToFeature(e) {
-    mymap.fitBounds(e.target.getBounds());
+  mymap.fitBounds(e.target.getBounds())
 }
 
 function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature,
+  })
 }
 
-var info = L.control();
+var info = L.control()
 
 info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-};
+  this._div = L.DomUtil.create('div', 'info') // create a div with a class "info"
+  this.update()
+  return this._div
+}
 
 // method that we will use to update the control based on feature properties passed
 info.update = function (props) {
-    this._div.innerHTML = '<h4>Name: </h4>' +  (props ?
-        '<b>' + props.name + '</b>'
-        : 'Hover over a state') + '<br>';
-};
-
-function loadJSON(path, success, error)
-{
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
+  this._div.innerHTML = '<h4>Name: </h4>' +
+    (props ? '<b>' + props.name + '</b>' : 'Hover over a state') + '<br>'
 }
 
-loadJSON('boundaries.json',
-    function(data) {
-        geojson = L.geoJSON(data, {
-            onEachFeature: onEachFeature
-        }).addTo(mymap);
+function loadJSON(path, success, error) {
+  var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        if (success) {
+          success(JSON.parse(xhr.responseText))
+        }
+      } else {
+        if (error) {
+          error(xhr)
+        }
+      }
+    }
+  }
+  xhr.open('GET', path, true)
+  xhr.send()
+}
 
-        geojson.eachLayer(function(layer) {
-            layer.setStyle({
-                fillColor: '#ff0000'
-            });
-        });
-        info.addTo(mymap);
-        init();
-    },
-    function(xhr) { console.error(xhr); }
-);
+loadJSON('boundaries.json', function (data) {
+  geojson = L.geoJSON(data, {
+    onEachFeature: onEachFeature,
+  }).addTo(mymap)
+
+  geojson.eachLayer(function (layer) {
+    layer.setStyle({
+      fillColor: '#ff0000',
+    })
+  })
+  info.addTo(mymap)
+  init()
+}, function (xhr) {
+  console.error(xhr)
+})
 
 function init() {
-    console.log('initializing...');
-    const selectWahl = document.querySelector('.select-wahl');
-    selectWahl.addEventListener('change', (event) => {
-        if (!event.target.value) {
-            return;
-        }
-        console.log('start loading');
-        loadJSON('elections/' + event.target.value + '.json',
-            function(data) {
-                console.log(event.target.value + ' loaded');
-            },
-            function(xhr) { console.error(xhr); }
-        );
-    });
+  console.log('Initializing...')
+  const selectWahl = document.querySelector('.select-wahl')
+  selectWahl.addEventListener('change', event => {
+    if (!event.target.value) {
+      return
+    }
+    console.log('start loading')
+    loadJSON('elections/' + event.target.value + '.json', function (data) {
+      console.log(event.target.value + ' loaded')
+    }, function (xhr) {
+      console.error(xhr)
+    })
+  })
 }
