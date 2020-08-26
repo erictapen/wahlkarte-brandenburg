ae1b8940bb3378d4180f6b2062dfc8c400c8b840
Justin Humm <justin.humm@posteo.de>
2020-08-26 19:25:21 +0200

    added some more elections, included script with source URLs

---
+++ wahlergebnisse-brandenburg/raw/fetch-raw.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# Europawahlen
+
+wget https://www.statistik-berlin-brandenburg.de/publikationen/dowmies/DL_BB_EU2019.xlsx
+wget https://www.wahlergebnisse.brandenburg.de/wahlen/EU2014/downloads/DL_BB_EU2014.xlsx
+
+# Bundestagswahlen
+
+wget https://www.wahlergebnisse.brandenburg.de/wahlen/BU2017/downloads/DL_BB_BU2017.xlsx
+wget https://www.wahlergebnisse.brandenburg.de/wahlen/BU2013/downloads/DL_BB_BU2013.xlsx
+
+# Landtagswahlen
+
+wget https://www.statistik-berlin-brandenburg.de/publikationen/dowmies/DL_BB_2_LT2019.xlsx
+wget https://www.wahlergebnisse.brandenburg.de/wahlen/LT2014/downloads/DL_BB_LT2014.xlsx
+
+# Kommunalwahlen
+
+wget https://www.statistik-berlin-brandenburg.de/publikationen/dowmies/DL_BB_2_KW2019.xlsx
+wget https://www.wahlergebnisse.brandenburg.de/wahlen/KO2014/downloads/DL_BB_KW2014.xlsx
+
