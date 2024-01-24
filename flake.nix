1795f344a88a9d611dbb4bf214a27a023d2b70a1
Kerstin Humm <kerstin@erictapen.name>
2024-01-24 22:48:20 +0100

    use flake-utils to build on multiple architectures

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/Nixpkgs/nixos-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
     brandenburg-pbf = {
       type = "git";
       url = "file:///home/kerstin/git/mmz/wahlkarte/generate-boundaries/raw";
@@ -11,90 +12,100 @@
   };
 
   outputs =
-    { self, nixpkgs, brandenburg-pbf }: rec {
+    { self, nixpkgs, flake-utils, brandenburg-pbf }:
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" "aarch64-linux" ]
+      (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
 
-      defaultPackage.x86_64-linux = packages.x86_64-linux.wahlkarte;
-
-      packages.x86_64-linux = let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-      in
-        rec {
-
-          generate-boundaries = (
-            import ./generate-boundaries/Cargo.nix {
-              inherit pkgs;
-            }
-          ).rootCrate.build;
-
-          wahlergebnisse-brandenburg = pkgs.runCommand "wahlergebnisse-brandenburg" {
-            src = ./wahlergebnisse-brandenburg;
-            buildInputs = with pkgs.python3.pkgs; [
-              openpyxl
-            ];
-          } ''
-            mkdir -p $out
-            python3 $src/generate.py $src/raw/ $out/
-          '';
-          boundaries = pkgs.runCommand "boundaries.json" {
-            buildInputs = [ generate-boundaries ];
-          } ''
-            generate-boundaries \
-              --ags-file ${wahlergebnisse-brandenburg}/eu2019.json \
-              --pbf-file ${brandenburg-pbf}/brandenburg-latest.osm.pbf $out
-          '';
-
-          wahlkarte = let
-            leaflet = rec {
-              version = "1.6.0";
-              js = pkgs.fetchurl {
-                url = "https://unpkg.com/leaflet@${version}/dist/leaflet.js";
-                sha256 = "sha256:1jnadhs874xis8gg91433m29yb1q15ijclmk7nc6pn0g16pi3nkw";
-              };
-              css = pkgs.fetchurl {
-                url = "https://unpkg.com/leaflet@${version}/dist/leaflet.css";
-                sha256 = "sha256:18vn46pcig8aw3yggpfzbcmjbl7z263a7811lf98wkwji44hcws8";
-              };
-            };
+        packages =
+          let
+            inherit (self.packages.${system})
+              boundaries wahlergebnisse-brandenburg generate-boundaries wahlkarte;
           in
-            pkgs.runCommand "wahlkarte" {
-              src = builtins.filterSource
-                (
-                  path: type:
-                    baseNameOf path != "generate-boundaries" && baseNameOf path != "wahlergebnisse-brandenburg"
-                )
-                ./.;
-            } ''
+          {
+
+            default = self.packages.${system}.wahlkarte;
+
+            generate-boundaries = (
+              import ./generate-boundaries/Cargo.nix {
+                inherit pkgs;
+              }
+            ).rootCrate.build;
+
+            wahlergebnisse-brandenburg = pkgs.runCommand "wahlergebnisse-brandenburg"
+              {
+                src = ./wahlergebnisse-brandenburg;
+                buildInputs = with pkgs.python3.pkgs; [
+                  openpyxl
+                ];
+              } ''
               mkdir -p $out
-              cp $src/index.html $out/
-              cp $src/wahlkarte.js $out/
-              mkdir -p $out/leaflet@${leaflet.version}/dist
-              ln -s ${leaflet.js}  $out/leaflet@${leaflet.version}/dist/leaflet.js
-              ln -s ${leaflet.css} $out/leaflet@${leaflet.version}/dist/leaflet.css
-              ln -s ${boundaries} $out/boundaries.json
-              ln -s ${wahlergebnisse-brandenburg} $out/elections
+              python3 $src/generate.py $src/raw/ $out/
+            '';
+            boundaries = pkgs.runCommand "boundaries.json"
+              {
+                buildInputs = [ generate-boundaries ];
+              } ''
+              generate-boundaries \
+                --ags-file ${wahlergebnisse-brandenburg}/eu2019.json \
+                --pbf-file ${brandenburg-pbf}/brandenburg-latest.osm.pbf $out
             '';
 
-          deploy = pkgs.writeScript "deploy-wahlkarte" ''
-            #! /usr/bin/env bash
-            ${pkgs.openssh}/bin/scp -r ${wahlkarte}/* mmz.erictapen.name:/webroot/mmz.erictapen.name/
-          '';
-        };
+            wahlkarte =
+              let
+                leaflet = rec {
+                  version = "1.6.0";
+                  js = pkgs.fetchurl {
+                    url = "https://unpkg.com/leaflet@${version}/dist/leaflet.js";
+                    sha256 = "sha256:1jnadhs874xis8gg91433m29yb1q15ijclmk7nc6pn0g16pi3nkw";
+                  };
+                  css = pkgs.fetchurl {
+                    url = "https://unpkg.com/leaflet@${version}/dist/leaflet.css";
+                    sha256 = "sha256:18vn46pcig8aw3yggpfzbcmjbl7z263a7811lf98wkwji44hcws8";
+                  };
+                };
+              in
+              pkgs.runCommand "wahlkarte"
+                {
+                  src = builtins.filterSource
+                    (
+                      path: type:
+                        baseNameOf path != "generate-boundaries" && baseNameOf path != "wahlergebnisse-brandenburg"
+                    )
+                    ./.;
+                } ''
+                mkdir -p $out
+                cp $src/index.html $out/
+                cp $src/wahlkarte.js $out/
+                mkdir -p $out/leaflet@${leaflet.version}/dist
+                ln -s ${leaflet.js}  $out/leaflet@${leaflet.version}/dist/leaflet.js
+                ln -s ${leaflet.css} $out/leaflet@${leaflet.version}/dist/leaflet.css
+                ln -s ${boundaries} $out/boundaries.json
+                ln -s ${wahlergebnisse-brandenburg} $out/elections
+              '';
 
-      devShell.x86_64-linux = let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-      in
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            darkhttpd
-            cargo
-            rustc
-            python3Packages.openpyxl
-          ];
-        };
-    };
+            deploy = pkgs.writeScript "deploy-wahlkarte" ''
+              #! /usr/bin/env bash
+              ${pkgs.openssh}/bin/scp -r ${wahlkarte}/* mmz.erictapen.name:/webroot/mmz.erictapen.name/
+            '';
+          };
+
+        devShell.x86_64-linux =
+          let
+            pkgs = import nixpkgs {
+              system = "x86_64-linux";
+            };
+          in
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              darkhttpd
+              cargo
+              rustc
+              python3Packages.openpyxl
+            ];
+          };
+      });
 
 }
